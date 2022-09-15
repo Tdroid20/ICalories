a5d0ae8c7a0f9416ad8d6d4881a484484991650d
Tdroid <garotorocha90@gmail.com>
2022-09-15 12:43:07 -0700

    Start ICalories

---
+++ ICalories/ICaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICaloriesApp.swift
+//  ICalories
+//
+//  Created by user226765 on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ICaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
